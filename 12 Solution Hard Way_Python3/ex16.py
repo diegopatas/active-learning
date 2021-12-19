9109709d3a621d059de0d44230e023b2585ebe34
Diego Diniz <oitopatas.email@gmail.com>
2021-12-18 22:35:50 -0300

    Update the code for the Exercise 16.

--- 12 Solution Hard Way_Python3/ex16.py
+++ 12 Solution Hard Way_Python3/ex16.py
@@ -1,12 +1,10 @@
+"""
+This program shows a file's content and gives the option to erase and/or insert new lines.
+"""
+
 from sys import argv
 
-def process_content(filename):
-    """Open and close a file."""
-    open_op = open(filename)
-    file_content = open_op.read()
-    open_op.close()
-    return  file_content
-
+# FUNCTIONS
 def erase_content(filename):
     """Erase the content of a file."""
     open_op = open(filename, 'w')
@@ -28,23 +26,22 @@ def show_content(filename):
 
 
 # MAIN PROGRAM
-
 script, filename = argv
 
 print(f"Welcome! This is the current content of the file: "
-      f"\n{filename.show_content()}")
+      f"\n{show_content(filename)}")
 
 resp = input("Do you want to erase the content? [Y]es or [N]o?")
 
 if resp.lower() == 'n':
-    filename.erase_content(filename)
+    erase_content(filename)
 
 while True:
     resp = input("Do you want to insert a line to the file? [Y]es or [N]o")
-    if resp.lower(filename) == 'n':
+    if resp.lower() == 'n':
         break
     else:
-        filename.write_content(filename)
+        write_content(filename)
 
 print(f"The final content of the file is: "
-      f"\n{filename.show_content(filename)}.")
+      f"\n{show_content(filename)}.")
